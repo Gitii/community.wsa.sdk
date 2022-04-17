7af8d81f6132e1dc22ad0e1b5b24005513c893a9
Gitii <Gitii@users.noreply.github.com>
2022-04-17 15:49:15 +0200

    Updated ci scripts

--- generate-coverage-report.cmd
+++
@@ -1,5 +0,0 @@
-generate-coverage.cmd
-
-rmdir /S /Q CoverageReport
-reportgenerator "-reports:.\CoverageResults\coverage.net6.0.cobertura.xml" "-targetdir:CoverageReport" "-reporttypes:Html;HtmlSummary"
-rmdir /S /Q CoverageResults
--- generate-coverage.sh
+++ generate-coverage.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-dotnet test /p:TargetFramework=net6.0 /p:CollectCoverage=true /p:Exclude=\"[Community.Wsl.Sdk.Tests]*\" /p:CoverletOutput=../CoverageResults/ /p:MergeWith="../CoverageResults/coverage.net6.0.json" /p:CoverletOutputFormat=\"cobertura,json\" /p:ThresholdType=\"line,branch,method\" -m:1
-
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=../CoverageResults/  /p:CoverletOutputFormat=cobertura /p:ThresholdType=\"line,branch,method\" /m:1
